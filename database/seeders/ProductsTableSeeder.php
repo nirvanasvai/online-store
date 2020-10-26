a5c9a8df72c6ac10473039d9ffbb2a105409baf2
stam <userovstam@gmail.com>
2020-10-27 01:09:48 +0600

    vol.2

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=1;$i<11;$i++)
+
+            DB::table('products')->insert([
+            'title' => 'Products',
+            'price' =>rand(100,2000),
+            'in_stock' => 1,
+            'description' =>'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose' ,
+        ]);
+    }
+}
